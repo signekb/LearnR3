31ff85817d632606915adf648c97fe8a0a18dc27
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-05-04 20:35:06 +0200

    feat: create dfs for data and summarise rr and actigraph data

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -18,10 +18,35 @@ Sys.sleep(1)
 unzip(here("data-raw/MMASH.zip"), exdir = here("data-raw"))
 
 # Remove/tidy up left over files
-
 file_delete(here(c(
   "data-raw/MMASH.zip",
   "data-raw/SHA256SUMS.txt",
   "data-raw/LICENSE.txt"
 )))
 file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
+
+# load data for all users
+user_info_df <- import_multiple_files("user_info.csv", import_user_info)
+saliva_df <- import_multiple_files("saliva.csv", import_saliva)
+rr_df <- import_multiple_files("RR.csv", import_rr)
+actigraph_df <- import_multiple_files("Actigraph.csv", import_actigraph)
+
+# summarise rr
+summarised_rr_df <- rr_df |>
+    group_by(file_path_id, day) |>
+    summarise(across(ibi_s, list(
+        mean = \(x) mean(x, na.rm = TRUE),
+        sd = \(x) sd(x, na.rm = TRUE)
+    ))) |>
+    ungroup()
+
+# summarise actigraph
+summarised_actigraph_df <- actigraph_df |>
+    group_by(file_path_id, day) |>
+    summarise(across(c(hr, steps), list(
+        mean = \(x) mean(x, na.rm = TRUE),
+        sd = \(x) sd(x, na.rm = TRUE),
+        max = \(x) max(x, na.rm = TRUE)
+    ))) |>
+    ungroup()
+# Code pasted here that was made from the above exercise
