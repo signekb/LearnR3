b91c2d84cff294677c7a82a72937c7835246b089
Signe Kirk Brødbæk <signekb@clin.au.dk>
2024-05-04 17:41:39 +0200

    feat: add import_multiple_files

--- R/functions.R
+++ R/functions.R
@@ -92,3 +92,20 @@ import_actigraph <- function(file_path) {
   )
   return(actigraph_data)
 }
+
+#' Import multiple MMASH data files and merge into one data frame.
+#'
+#' @param file_pattern Pattern for which data file to import.
+#' @param import_function Function to import the data file.
+#'
+#' @return A single data frame/tibble.
+#'
+import_multiple_files <- function(file_pattern, import_function) {
+  data_files <- fs::dir_ls(here::here("data-raw/mmash/"),
+    regexp = file_pattern,
+    recurse = TRUE
+  )
+  combined_data <- purrr::map(data_files, import_function) |>
+    purrr::list_rbind(names_to = "file_path_id")
+  return(combined_data)
+}
